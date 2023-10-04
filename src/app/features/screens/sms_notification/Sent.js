f7c83397cc0ffec50db887b3ed10998a619b9a2d
hamza akram <hamzaakram587@gmail.com>
2023-10-05 02:03:41 +0500

    latest code

--- src/app/features/screens/sms_notification/Sent.js
+++ src/app/features/screens/sms_notification/Sent.js
@@ -8,10 +8,13 @@ import CustomLoading from "../../components/custom_spinner";
 import { getSentAllTemplateAction } from "../../../store/notification/NotificationAction";
 import Axios from "axios";
 import BASE_URL from "../../../../config";
+import ReactPaginate from 'react-paginate';
+import TemplateSearchBar from "./TemplateSearchBar";
 
 const Sent = () => {
     const [allSent, setAllSent] = useState([])
-
+    const [currentPage, setCurrentPage] = useState(0); // Add currentPage state
+    const [searchResults, setSearchResults] = useState([]);
     const loginResponse = useSelector((state) => state.login)
     const loginData = loginResponse.loginData
     const companyId = loginData?.compony
@@ -49,6 +52,31 @@ const Sent = () => {
     const type = (rowData) => {
         return <div>{rowData.type === 0 ? "SMS" : rowData.type === 1 ? "Email" : "SMS, Email"}</div>;
     };
+
+    // Constants for pagination
+  const itemsPerPage = 10;
+  const pageCount = Math.ceil(allSent.length / itemsPerPage);
+  const offset = currentPage * itemsPerPage;
+
+  // Function to handle page change
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+   // Function to handle the search
+   const handleSearch = (searchTerm) => {
+    // Implement your search logic here
+    const filteredResults = allSent.filter((template) => {
+      return (
+        template.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        template.templateId.toString().includes(searchTerm)
+      );
+    });
+    setSearchResults(filteredResults);
+  };
+
+  // Render the visible items based on the current page
+  const visibleItems = allSent.slice(offset, offset + itemsPerPage);
+
     const createdAtFormatted = (rowData) => {
         const createdAtDate = new Date(rowData.createdAt);
         const options = {
@@ -64,15 +92,21 @@ const Sent = () => {
 
     return (
         <div className="card bg-pink-50">
+            <div className="flex bar-place">
             <div className="mx-5">
                 <h3 className="text-xl font-semibold border-bottom-1 pb-2">Sent</h3>
             </div>
+            <div className=" mb-3">
+        <TemplateSearchBar onSearch={handleSearch} />
+      </div>
+            </div>
+           
             <div className="card mx-5 p-0 border-noround">
                 {getSentAllTemplateLoading ? (
                     <CustomLoading />
                 ) : (
                     <div className="">
-                        <DataTable value={allSent} showGridlines>
+                        <DataTable value={searchResults.length > 0 ? searchResults : visibleItems} showGridlines>
                             <Column header="Template Id" field="templateId" />
                             <Column header="Name" field="name" />
                             <Column header="Message" field="template" />
@@ -83,6 +117,15 @@ const Sent = () => {
                             <Column header="Status" field="status" />
                             <Column header="Actions" body={renderActions} style={{ width: "50px" }} />
                         </DataTable>
+                        <ReactPaginate
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
+              breakLabel={"..."}
+              pageCount={pageCount}
+              onPageChange={handlePageClick}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+            />
                     </div>
                 )}
             </div>
