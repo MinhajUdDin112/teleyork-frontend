3db0a6e3288759707a65eaff3f199b0ca8d9f33a
Zaka Ullah <mrzakaullah01@gmail.com>
2024-02-06 19:31:07 +0500

    Billing For Customer Set Up

--- src/app/features/screens/billing_and_invoices/components/InvoiceTable.js
+++ src/app/features/screens/billing_and_invoices/components/InvoiceTable.js
@@ -2,62 +2,132 @@ import React from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
-
-const InvoiceTable = ({ setDetailedTransactionModal,invoiceData }) => {
-    const cardData = invoiceData
+import "./css/invoicetable.css";
+const InvoiceTable = ({ setDetailedTransactionModal, invoiceData }) => {
+    const cardData = invoiceData;
     const handleCellClick = () => {
         setDetailedTransactionModal(true);
     };
-
+    const rowClassName = (rowData) => {
+        // Example condition: apply different classes based on status
+        if (rowData.invoiceStatus === "Paid") {
+            return "paid-invoice";
+        } else {
+            return "unpaid-invoice"; // No class
+        }
+    };
     return (
         <div className="mx-4">
-            <DataTable value={cardData} showGridlines>
+            <DataTable value={cardData} rowClassName={rowClassName}>
                 <Column
                     InvoiceTable
-                    field="invoiceNumber"
+                    field="invoiceNo"
                     header="Invoice No."
-                    style={{ minWidth: "150px" }}
+                    style={{ minWidth: "250px" }}
                     body={(rowData) => (
                         <span style={{ cursor: "pointer" }} onClick={() => handleCellClick()}>
-                            {rowData.invoiceNumber}
+                            {rowData?.invoiceNo}
                         </span>
                     )}
                 />
-                <Column field="type" header="Type" style={{ minWidth: "150px" }} />
-                <Column field="Debit" header="Debit" style={{ minWidth: "150px" }} />
-                <Column field="Tax" header="Tax" style={{ minWidth: "150px" }} />
-                <Column field="Processing Fee" header="Processing" style={{ minWidth: "150px" }} />
-                <Column field="Invoice Balance" header="Inv Balance" style={{ minWidth: "150px" }} />
-                <Column field="Amount Paid" header="Amount Paid" style={{ minWidth: "150px" }} />
-                <Column field="Total Dues" header="Total Dues" style={{ minWidth: "150px" }} />
-                <Column field="From Date" header="From Date" style={{ minWidth: "150px" }} />
-                <Column field="Due Date" header="Due Date" style={{ minWidth: "150px" }} />
-                <Column field="createdAt" header="Created Date" style={{ minWidth: "150px" }} />
-                <Column field="Created By" header="Created By" style={{ minWidth: "150px" }} />
-                <Column field="Source" header="Source" style={{ minWidth: "150px" }} />
-                <Column field="Paid Using" body={(rowData)=>{ 
-                     return "Credit/Debit Card"
-                }} header="Paid Using" style={{ minWidth: "150px" }} />
-                <Column field="status" header="status" style={{ minWidth: "150px" }} />
+
+                <Column field="invoiceType" header="Invoice Type" style={{ minWidth: "250px" }} />
+                <Column field="invoiceOneTimeCharges" header="Invoice One Time Charges" style={{ minWidth: "250px" }} />
+                <Column
+                    field="additionalCharges"
+                    header="Additional Charges"
+                    body={(rowData) => {
+                        let additional = "";
+                        for (let i = 0; i < rowData?.additionalCharges.length; i++) {
+                            if (i + 1 === rowData?.additionalCharges.length) {
+                                let name = rowData?.additionalCharges[i].name + ":" + rowData.additionalCharges[i].amount;
+                                additional += name;
+                            } else {
+                                let name = rowData?.additionalCharges[i].name + ":" + rowData.additionalCharges[i].amount + ",";
+                                additional += name;
+                            }
+                        }
+                        return <p>{additional}</p>;
+                    }}
+                    style={{ minWidth: "350px" }}
+                />
+                <Column
+                    field="discount"
+                    header="Discounts"
+                    style={{ minWidth: "250px" }}
+                    body={(rowData) => {
+                        let discount = "";
+                        for (let i = 0; i < rowData?.discount.length; i++) {
+                            if (i + 1 === rowData.discount.length) {
+                                let name = rowData.discount[i].name + ":" + rowData.discount[i].amount;
+                                discount += name;
+                            } else {
+                                let name = rowData.discount[i].name + ":" + rowData.discount[i].amount + ",";
+                                discount += name;
+                            }
+                        }
+                        return <p>{discount}</p>;
+                    }}
+                />
+                <Column field="planCharges" header="Plan Charges" style={{ minWidth: "250px" }} />
+                <Column field="totalAmount" header="Total Amount" style={{ minWidth: "250px" }} />
+                <Column field="amountPaid" header="Paid Amount" style={{ minWidth: "250px" }} />
+                <Column field="lateFee" header="Late Fee" style={{ minWidth: "250px" }} />
+                <Column field="invoiceDueDate" header="DueDate" style={{ minWidth: "250px" }} />
+                <Column field="invoiceStatus" header="Status" style={{ minWidth: "250px" }} />
+                <Column field="invoicePaymentMethod" header="Payment Method" style={{ minWidth: "250px" }} />
+                <Column
+                    field="BillingPeriod"
+                    header="Billing Period"
+                    body={(rowData) => {
+                        console.log(rowData.billingPeriod);
+                        let billingperiod = "From :" + rowData.billingPeriod.from + "," + " To:" + rowData.billingPeriod.to;
+
+                        return <p>{billingperiod}</p>;
+                    }}
+                    style={{ minWidth: "350px" }}
+                />
+
                 <Column
                     field="Action"
                     body={
-                        <Button className="bg-green-200 border-none" onClick={() => setDetailedTransactionModal(true)}>
+                        <Button className="bg-green-700 pl-2 pr-2 pt-1 pb-1 border-none" onClick={() => setDetailedTransactionModal(true)}>
                             Void
                         </Button>
                     }
-                    header="Action (PC609)"
-                    style={{ minWidth: "150px" }}
+                    header="Download "
+                    style={{ minWidth: "250px" }}
                 />
+                <Column
+                    field="Invoice Refund"
+                    body={
+                        <Button className="bg-green-700 pl-2 pr-2 pt-1 pb-1 border-none" onClick={() => setDetailedTransactionModal(true)}>
+                            Refund{" "}
+                        </Button>
+                    }
+                    header="Invoice Refund"
+                    style={{ minWidth: "250px" }}
+                />
+                <Column
+                    field="Invoice_Ebill"
+                    body={
+                        <Button className="bg-green-700 pl-2 pr-2 pt-1 pb-1 border-none" onClick={() => setDetailedTransactionModal(true)}>
+                            Ebill{" "}
+                        </Button>
+                    }
+                    header="Invoice EBill"
+                    style={{ minWidth: "250px" }}
+                />
+
                 <Column
                     field="Invoice_Pdf"
                     body={
-                        <Button className="bg-green-200 border-none" onClick={() => setDetailedTransactionModal(true)}>
+                        <Button className="bg-green-700  pl-2 pr-2 pt-1 pb-1 border-none" onClick={() => setDetailedTransactionModal(true)}>
                             Download{" "}
                         </Button>
                     }
                     header="Invoice Pdf"
-                    style={{ minWidth: "150px" }}
+                    style={{ minWidth: "250px" }}
                 />
             </DataTable>
         </div>
--- src/app/features/screens/billing_and_invoices/components/PrepaidPlanInfo.js
+++ src/app/features/screens/billing_and_invoices/components/PrepaidPlanInfo.js
@@ -1,89 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const selectedid = localStorage.getItem("selectedId");
+const parseselectedid = JSON.parse(selectedid);
+const PlanInfo = () => {
+    const [currentPlan, setCurrentPlan] = useState({});
+    const [additional, setAdditional] = useState();
+    const [discount, setDiscount] = useState();
+    useEffect(async () => {
+        Axios.get(`${BASE_URL}/api/web/invoices/getplanbycustomerid?customerid=${parseselectedid}`)
+            .then((res) => {
+                console.log("current plan is ", res);
+                let additionalnew = "";
+                for (let i = 0; i < res?.data?.data?.currentPlan.additionalCharges.length; i++) {
+                    if (i + 1 === res?.data?.data?.currentPlan.additionalCharges.length) {
+                        let name = res?.data?.data?.currentPlan.additionalCharges[i].name + ":" + res?.data?.data?.currentPlan.additionalCharges[i].amount;
+                        additionalnew += name;
+                    } else {
+                        let name = res?.data?.data?.currentPlan.additionalCharges[i].name + ":" + res?.data?.data?.currentPlan.additionalCharges[i].amount + ",";
+                        additionalnew += name;
+                    }
+                }
+                let discountnew = "";
+                for (let i = 0; i < res?.data?.data?.currentPlan.discount.length; i++) {
+                    if (i + 1 === res?.data?.data?.currentPlan.discount.length) {
+                        let name = res?.data?.data?.currentPlan.discount[i].name + ":" + res?.data?.data?.currentPlan.discount[i].amount;
+                        discountnew += name;
+                    } else {
+                        let name = res?.data?.data?.currentPlan.discount[i].name + ":" + res?.data?.data?.currentPlan.discount[i].amount + ",";
+                        discountnew += name;
+                    }
+                }
+                setDiscount(discountnew);
+                setAdditional(additionalnew);
 
-const PlanInfo = ({currentPlan}) => { 
-    let feature="" 
-    let discounts=""
-    let additionalFeature=currentPlan[0]?.additionalFeature   
-    let discount=currentPlan[0]?.discount
-    for(let i=0;i<discount?.length;i++){  
-         if(i+1 === discount?.length){
-       discounts+=`${discount[i]?.discountname}`;  
-         } 
-         else{ 
-            
-       discounts+=`${discount[i]?.discountname},`;  }
-         }   
-         for(let i=0;i<additionalFeature?.length;i++){  
-            if(i+1 === additionalFeature?.length){
-            feature+=`${additionalFeature[i]?.featureName}`;  
-            } 
-            else{ 
-               
-          feature+=`${additionalFeature[i]?.featureName},`;  
-            }
-        
-
-    }
+                setCurrentPlan(res.data.data.currentPlan);
+            })
+            .catch((err) => {});
+    }, []);
     return (
         <div className=" ">
             <div className="flex flex-wrap mx-5">
                 <div className="col-3 ">
                     <div className="mb-1">
                         <p className="font-bold text-sm m-0">Current Plan Name:</p>
-                        <p className="text-sm m-0">{currentPlan[0]?.plan[0]?.name}</p>
+                        <p className="text-sm m-0">{currentPlan?.planName}</p>
                     </div>
                     <div className="mb-1">
-                        <p className="font-bold text-sm m-0">Bill Generate:</p>
-                        <p className="text-sm m-0">{currentPlan[0]?.billingPeriod}</p>
+                        <p className="font-bold text-sm m-0">Current Bill Cycle Day:</p>
+                        <p className="text-sm m-0">
+                            From:{currentPlan?.billingPeriod?.from},To:{currentPlan?.billingPeriod?.to}
+                        </p>
                     </div>
-                  
                 </div>
                 <div className="col-3">
                     <div className="mb-1">
-                        <p className="font-bold text-sm m-0">Current Plan Price($):</p>
-                        <p className="text-sm m-0">{currentPlan[0]?.plan[0]?.price}</p>
-                    </div> 
-                    <div className="mb-1">
-                        <p className="font-bold text-sm m-0">Current Deposit Amount:</p>
-                        <p className="text-sm m-0">{currentPlan[0]?.totalAmount}</p>
+                        <p className="font-bold text-sm m-0">Current Plan Price:</p>
+                        <p className="text-sm m-0">{currentPlan?.planCharges}</p>
                     </div>
-                    {/*<div className="mb-1">
-                        <p className="font-bold text-sm m-0">Current credit Limit:</p>
-                        <p className="text-sm m-0">500</p>
-                    </div>   */}
-                    {/*<div className="mb-1">
-                        <p className="font-bold text-sm m-0">Due date day is:</p>
-                        <p className="text-sm m-0">4th of month</p>  
-                         
-                    </div>*/ 
-                    } 
-                                    </div>
+                    <div className="mb-1">
+                        <p className="font-bold text-sm m-0">Print Setting:</p>
+                        <p className="text-sm m-0">{currentPlan?.printSetting}</p>
+                    </div>
+                </div>
                 <div className="col-3">
                     <div className="mb-1">
                         <p className="font-bold text-sm m-0">Charging type:</p>
-                        <p className="text-sm m-0">Monthly</p>
+                        <p className="text-sm m-0">{currentPlan?.chargingType}</p>
                     </div>
-                    {/*<div className="mb-1">
-                        <p className="font-bold text-sm m-0">Consumed Credit:</p>
-                        <p className="text-sm m-0">365</p>
-                    </div>     */}
-                   
+                    <div className="mb-1">
+                        <p className="font-bold text-sm m-0">Due Date Is:</p>
+                        <p className="text-sm m-0">{currentPlan?.invoiceDueDate}</p>
+                    </div>
                 </div>
                 <div className="col-3">
                     <div className="mb-1">
                         <p className="font-bold text-sm m-0">Additional Feature:</p>
-                        <p className="text-sm m-0">{feature}</p>
-                    </div> 
+                        <p className="text-sm m-0">{additional}</p>
+                    </div>
                     <div className="mb-1">
                         <p className="font-bold text-sm m-0">Discounts:</p>
-                        <p className="text-sm m-0">{discounts}</p>
+                        <p className="text-sm m-0">{discount}</p>
                     </div>
-                  {/*  <div className="mb-1">
+                    <div className="mb-1">
+                        <p className="font-bold text-sm m-0">Apply Late Fee:</p>
+                        <p className="text-sm m-0">{currentPlan?.invoiceDueDate}</p>
+                    </div>
+                    {/*  <div className="mb-1">
                         <p className="font-bold text-sm m-0">Available Postpaid Limit:</p>
                         <p className="text-sm m-0">765</p>
                     </div>  
-                    */ 
-                }
+                    */}
                 </div>
             </div>
         </div>
