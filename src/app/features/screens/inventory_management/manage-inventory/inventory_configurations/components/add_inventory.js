8b4c281f9f79ce594d4c637491bd773412da8c3b
Zaka Ullah <mrzakaullah01@gmail.com>
2024-02-22 20:53:40 +0500

    Inventory Type Configuration

---
+++ src/app/features/screens/inventory_management/manage-inventory/inventory_configurations/components/add_inventory.js
@@ -0,0 +1,59 @@
+import React from "react";  
+import { useFormik } from "formik"; 
+import * as Yup from "yup"; 
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button"; 
+import { useRef } from "react";
+import { Toast } from "primereact/toast";
+import Axios from "axios";
+const validationSchema = Yup.object().shape({
+    inventoryType: Yup.string().required("Inventory Type Is Required"),
+})  
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const loginRes = localStorage.getItem("userData");
+const parseLoginRes = JSON.parse(loginRes);
+export default function AddInventory({setRefresh, setAddInventoryVisibility}){ 
+    const toast = useRef(null);
+    const formik=useFormik({ 
+        initialValues:{ 
+         inventoryType:"", 
+         serviceProvider:parseLoginRes?.company
+        }, 
+        validationSchema, 
+        onSubmit:(values)=>{ 
+       Axios.post(`${BASE_URL}/api/inventoryType/add`,formik.values).then(()=>{
+        toast.current.show({ severity: "success", summary: "Inventory Type Added Successfully", detail: "Plan Updated Successfully" });
+        setTimeout(() => {
+            setAddInventoryVisibility(false); 
+            setRefresh(prev=>!prev)
+        }, 1000);
+       }).catch(err=>{ 
+        toast.current.show({ severity: "error", summary: "Plan Updation", detail: "Plan Updation Failed" });
+            
+       })
+        }
+    })
+    return(   
+         
+         <div className="flex flex-wrap justify-content-around flex-row"> 
+          <form onSubmit={formik.handleSubmit}>
+           <div className="w-full     flex flex-wrap justify-content-around flex-row  "> 
+           <div className="mt-0">
+                        <label className="block">
+                            Inventory Type <span className="star">*</span>
+                        </label>
+                        <InputText className="field-width mt-2" name="inventoryType" value={formik.values.inventoryType} onChange={formik.handleChange} />
+                        {formik.touched.inventoryType && formik.errors.inventoryType? <p className="mt-2 ml-1 star">{formik.errors.inventoryType}</p> : null}
+                    </div>
+           </div>  
+           <div className="w-full  mt-4    flex flex-wrap justify-content-around flex-row  "> 
+           <div className="mt-0">
+                         <Button className="field-width" label="Submit" type="submit"/>
+                      </div>
+           </div> 
+            </form> 
+            
+            <Toast ref={toast} />
+         </div>
+    )
+}
---
+++ src/app/features/screens/inventory_management/manage-inventory/inventory_configurations/inventory_configurations.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import Axios from "axios";
+import { Card } from "primereact/card";
+import UpdateInventory from "./components/update_invenory";
+import { ToastContainer } from "react-toastify";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { toast } from "react-toastify";
+import AddInventory from "./components/add_inventory";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const loginRes = localStorage.getItem("userData");
+const parseLoginRes = JSON.parse(loginRes);
+export default function ListAllInventories() {
+    console.log("All Inventories Listed");
+    const [refresh, setRefresh] = useState(false);
+    const [editInventoryVisibility, setEditInventoryVisibility] = useState(false);
+    useEffect(() => {
+        if (editInventoryVisibility === false) {
+            Axios.get(`${BASE_URL}/api/inventoryType/all?serviceProvider=${parseLoginRes?.company}`)
+                .then((res) => {
+                    setInventoryList(res?.data?.data);
+                })
+                .catch((err) => {});
+        }
+        if (addInventoryVisibility === false) {
+            Axios.get(`${BASE_URL}/api/inventoryType/all?serviceProvider=${parseLoginRes?.company}`)
+                .then((res) => {
+                    setInventoryList(res?.data?.data);
+                })
+                .catch((err) => {});
+        }
+    }, [refresh]);
+    const [inventoryList, setInventoryList] = useState([]);
+    const [rowData, setRowData] = useState(null);
+    const [addInventoryVisibility, setAddInventoryVisibility] = useState(false);
+    return (
+        <div className="overflow-hidden">
+            <ToastContainer />
+            <Button
+                onClick={() => {
+                    setAddInventoryVisibility(true);
+                }}
+                className="text-center mr-0"
+                style={{ marginTop: "0px", textAlign: "center", marginLeft: "90%", transform: "translate(-70%)", width: "250px" }}
+                label="Add Inventory Type"
+            />
+
+            <Dialog
+                header="Add Inventory Type"
+                visible={addInventoryVisibility}
+                style={{ width: "80vw" }}
+                onHide={() => {
+                    setAddInventoryVisibility(false);
+                }}
+            >
+                <AddInventory setRefresh={setRefresh} setAddInventoryVisibility={setAddInventoryVisibility} />
+            </Dialog>
+            <Dialog
+                header="Update Inventory Type"
+                visible={editInventoryVisibility}
+                className="pt-0"
+                style={{ width: "80vw" }}
+                onHide={() => {
+                    setEditInventoryVisibility(false);
+                }}
+            >
+                <UpdateInventory data={rowData} setRefresh={setRefresh} setEditInventoryVisibility={setEditInventoryVisibility} />
+            </Dialog>
+            <DataTable value={inventoryList} size="small" stripedRows resizableColumns emptyMessage="No Inventory found." style={{ marginTop: "10px" }}>
+                <Column header="ID" field="_id" />
+                <Column header="Inventory Type" field="inventoryType" />
+
+                <Column
+                    header="Actions"
+                    body={(rowData) => {
+                        return (
+                            <>
+                                <Button
+                                    label="Update"
+                                    onClick={() => {
+                                        setRowData(rowData);
+                                        setEditInventoryVisibility(true);
+                                    }}
+                                    className=" p-button-primary mr-2 ml-2 pt-1 pb-1"
+                                    text
+                                    raised
+                                />
+                                <Button
+                                    label="Delete"
+                                    onClick={() => {
+                                        Axios.put(`${BASE_URL}/api/inventoryType/delete?id=${rowData._id}`)
+                                            .then((res) => {
+                                                toast.success("Inventory Type Removed Successdully");
+
+                                                setRefresh((prev) => !prev);
+                                            })
+                                            .catch((err) => {
+                                                toast.error("Inventory Type Removal Failed");
+                                            });
+                                    }}
+                                    className=" p-button-primary mr-2 ml-2 pt-1 pb-1"
+                                    text
+                                    raised
+                                />
+                            </>
+                        );
+                    }}
+                    field="Edit"
+                />
+            </DataTable>
+        </div>
+    );
+}
