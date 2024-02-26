f1cef1218b559eb2d96de008a98ed3bb58414ce4
Hamza Akram <hamzaakram587@gmail.com>
2024-02-26 22:45:28 +0500

    adhoc invoice

--- src/app/features/screens/billing_and_invoices/components/modals/AdHocModal.js
+++ src/app/features/screens/billing_and_invoices/components/modals/AdHocModal.js
@@ -1,123 +1,179 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
 import { Dropdown } from "primereact/dropdown";
 import { RadioButton } from "primereact/radiobutton";
 import { InputText } from "primereact/inputtext";
-import { useFormik } from "formik";
+import { useFormik, validateYupSchema } from "formik";
 import * as Yup from "yup";
 import Axios from 'axios';
 import { ToastContainer, toast } from "react-toastify"; // Import ToastContainer and toast
 import "react-toastify/dist/ReactToastify.css"; // Import toast styles
+import { Calendar } from "primereact/calendar";
+import classNames from "classnames";
 
-const AdHocModal = ({ adHocInvoiceModal, setAdHocInvoiceModal }) => {   
-    const BASE_URL=process.env.REACT_APP_BASE_URL
+const AdHocModal = ({ cpData, adHocInvoiceModal, setAdHocInvoiceModal }) => {
+    const BASE_URL = process.env.REACT_APP_BASE_URL
     const [selectedCity1, setSelectedCity1] = useState(null);
     const [city, setCity] = useState("");
     const [isButtonLoading, setisButtonLoading] = useState(false)
+    const [addNewType, setAddNewType] = useState(false)
+    const [allType, setAllType] = useState()
 
     const validationSchema = Yup.object().shape({
-        invoiceAmount: Yup.string().required("Please Select Invoice Amount"),  
-        invoiceType: Yup.string().required("Please select Invoice Type"), 
+        invoiceAmount: Yup.string().required("Please Select Invoice Amount"),
+        invoiceType: Yup.string().required("Please select Invoice Type"),
         amount: Yup.string().required("Please select Amount"),
-        isTax: Yup.string().required("Please select isTax"), 
-        taxAmount: Yup.string().required("Please Enter Tax Amount"), 
-        taxBreakup: Yup.string().required("Please Enter Tax Breakup"), 
+        isTax: Yup.string().required("Please select isTax"),
+        taxAmount: Yup.string().required("Please Enter Tax Amount"),
+        taxBreakup: Yup.string().required("Please Enter Tax Breakup"),
+        dueDate:Yup.string().required("Please Enter Due Date"),
     });
-    
+
     const formik = useFormik({
-        validationSchema: validationSchema,
-        initialValues: { 
-            invoiceAmount:"",    
-            invoiceType: "",  
+        validationSchema: validationSchema, 
+        initialValues: {
+            invoiceAmount: "",
+            invoiceType: "",
             isTax: "",
-            invoiceAmount:"",    
-            taxAmount: "",  
+            invoiceAmount: "",
+            taxAmount: "",
             taxBreakup: "",
+            typeName: "",
+            dueDate:""
         },
-        onSubmit: async (values,actions) => {
-          setisButtonLoading(true);
-            const dataToSend = {  ...values };
-        
-            // try {
-            //     const response = await Axios.post(`${BASE_URL}/api/user/activateByPwg`, dataToSend);
-            //     if (response?.status === 201 || response?.status === 200) {
-            //         toast.success("Successfully Run");
-            //         setisButtonLoading(false);
-            //         actions.resetForm();
-            //     }
-            // } catch (error) {
-            //   toast.error(error?.response?.data?.msg);
-            //   setisButtonLoading(false);
-            // }
+        onSubmit: async (values, actions) => {
+            setisButtonLoading(true);
+            console.log("Here is ")
+            const dataToSend = {
+
+             };
+
+            try {
+                const response = await Axios.post(`${BASE_URL}/api/user/web`, dataToSend);
+                if (response?.status === 201 || response?.status === 200) {
+                    toast.success("Successfully Run");
+                    setisButtonLoading(false);
+                    actions.resetForm();
+                }
+            } catch (error) {
+              toast.error(error?.response?.data?.msg);
+              setisButtonLoading(false);
+            }
+            setAdHocInvoiceModal(false)
         },
     });
-    
+    const loginRes = localStorage.getItem("userData");
+    const parseLoginRes = JSON.parse(loginRes);
+    const company = parseLoginRes?.company;
 
     const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
-const getFormErrorMessage = (name) => {
-    return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
-};
-    const cities = [
-        { name: "ace-cash-express", code: "NY" },
-        { name: "ACE-PAYMENT", code: "RM" },
-        { name: "Ad-hoc-Invoice (Other)", code: "LDN" },
-        { name: "Agent add minutes", code: "RM" },
-        { name: "Agent add minutes cash", code: "LDN" },
-    ];
-    const onCityChange = (e) => {
-        setSelectedCity1(e.value);
+    const getFormErrorMessage = (name) => {
+        return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
     };
-    const renderFooter = () => {
-        return (
-            <div className="flex justify-content-between m-3">    
-                <Button label="Close" onClick={() => setAdHocInvoiceModal(false)} />
-                <Button label="Submit"  onClick={() => setAdHocInvoiceModal(false)} />
-            </div>
-        );
+
+   
+    const getType = async () => {
+        try {
+            const res = await Axios.get(`${BASE_URL}/api/web/invoiceType/all?serviceProvider=${company}`);
+            setAllType(res?.data?.data || []);
+        } catch (error) {
+            toast.error(error?.response?.data?.msg);
+        }
     };
+    const addType = async () => {
+        const dataToSend = {
+            typeName: formik.values.typeName,
+            serviceProvider: company
+        }
+        try {
+            const response = await Axios.post(`${BASE_URL}/api/web/invoiceType/add`, dataToSend)
+         
+            if (response?.status == "201" || response?.status == "200") {
+                toast.success("Type add Successfully")
+            }
+        } catch (error) {
+            toast.error(error?.response?.data?.msg)
+        }
+        setAddNewType(false)
+
+    }
+    useEffect(() => {
+        getType();
+    }, [addNewType])
+
     return (
+        <>
+        <form onSubmit={formik.handleSubmit}>
         <div>
-            <Dialog header="Add New Invoice" headerStyle={{ borderBottom: "1px solid #c5c5c5", paddingLeft: "35px" }} closable={false} visible={adHocInvoiceModal} footer={renderFooter()} style={{ width: "50vw" }}>
+            <ToastContainer />
                 <div className="m-3">
                     <div className="flex">
                         <p className="col-4 font-semibold m-0 p-1">CustomerID:</p>
-                        <p className="col-8 m-0 p-1"><h5>119350</h5></p>
+                        <p className="col-8 m-0 p-1">{cpData?.accountId}</p>
                     </div>
-                    <div className="flex">
+                    <div className="flex mt-2">
                         <p className="col-4 font-semibold m-0 p-1">Telephone Number:</p>
-                        <p className="col-8 m-0 p-1"><h5>0313-55***52</h5></p>
+                        <p className="col-8 m-0 p-1">{cpData?.contact}</p>
                     </div>
-                    <div className="flex">
+                    <div className="flex  mt-2">
                         <p className="col-4 font-semibold m-0 p-1">Customer Name:</p>
-                        <p className="col-8 m-0 p-1"><h5>Hammad Ullah</h5></p>
+                        <p className="col-8 m-0 p-1">{cpData?.firstName} {cpData?.lastName}</p>
                     </div>
-                    <div className="flex">
+                    <div className="flex  mt-2">
                         <p className="col-4 font-semibold m-0 p-1">Address:</p>
-                        <p className="col-8 m-0 p-1"><h6>Islamabad Pakistan</h6></p>
+                        <p className="col-8 m-0 p-1">{cpData?.address1} {cpData?.address2}</p>
                     </div>
-                    <div className="flex">
-                        <p className="col-4 font-semibold m-0 p-1">Invoice Type:</p>
+                    <div className="flex  mt-2">
+                        <p className="col-4 font-semibold m-0 p-1">Select Type OR <span onClick={() => setAddNewType(true)} style={{ color: "blue", cursor: "pointer" }}>
+                            Add Type
+                        </span>{" "}</p>
                         <p className="col-8 m-0 p-1">
-                            <Dropdown placeholder="Select" id="invoiceType" value={formik.values.invoiceType} options={cities} onChange={formik.handleChange} optionLabel="name" optionValue="" className="h-3rem w-7 align-items-center " filter filterBy="name" />
+                            <Dropdown placeholder="Invoice Type" id="invoiceType" value={formik.values.invoiceType} options={allType}  onBlur={formik.handleBlur} onChange={formik.handleChange} filter filterBy="typeName" optionLabel="typeName" optionValue="_id"  className={classNames({ "p-invalid": isFormFieldValid("invoiceType") }, "h-3rem w-21rem align-items-center mr-3")} />
+                            {getFormErrorMessage("invoiceType")}
                         </p>
+                        
                     </div>
+                    {addNewType ? (
+
+                        <div className="flex">
+
+                            <p className="col-4 font-semibold m-0 p-1">Name:</p>
+                            <div className="flex">
+
+                                <InputText id="typeName" placeholder="Type Name" value={formik.values.typeName} onChange={formik.handleChange} className="h-3rem ml-1 border-round-xs w-21rem" />
+                                <i className="pi pi-check ml-2 " style={{ color: "green", fontSize: "24px", cursor: "pointer" }} onClick={addType}></i>
+                            </div>
+
+                        </div>
+
+                    ) : (
+                        " "
+                    )}
                     <div className="flex">
                         <p className="col-4 font-semibold m-0 p-1">Amount($):</p>
                         <p className="col-8 m-0 p-1">
-                            <InputText placeholder="Enter Amount" value={formik.values.amount}  onChange={formik.handleChange} className="h-3rem w-7 border-round-xs" />
+                            <InputText placeholder="Enter Amount" onBlur={formik.handleBlur} value={formik.values.amount} onChange={formik.handleChange} className={classNames({ "p-invalid": isFormFieldValid("amount") }, "h-3rem w-21rem border-round-xs mr-3")} />
+                            {getFormErrorMessage("amount")}
+                        </p>
+                    </div>
+                    <div className="flex">
+                        <p className="col-4 font-semibold m-0 p-1">Due Date:</p>
+                        <p className="col-8 m-0 p-1">
+                        <Calendar id="dueDate" value={formik.values.dueDate} onChange={formik.handleChange} showIcon style={{ width: "21rem" }} onBlur={formik.handleBlur} className={classNames({ "p-invalid": isFormFieldValid("dueDate") }, " mr-3")} />
+                            {getFormErrorMessage("dueDate")}
                         </p>
                     </div>
                     <div className="flex mt-2 mb-2">
                         <p className="col-4 font-semibold m-0 p-1">Do You want to include tax?:</p>
                         <p className="col-8 m-0 p-1">
                             <div>
-      
-                            
-                   
-                   
 
-                   <RadioButton inputId="isTax" name="pizza" value="yes" onChange={(e) => setCity(e.value)} checked={city === "yes"} />
+
+
+
+
+                                <RadioButton inputId="isTax" name="pizza" value="yes" onChange={(e) => setCity(e.value)} checked={city === "yes"} />
                                 <label htmlFor="isTax" className="mr-2 px-2">
                                     Yes
                                 </label>
@@ -131,24 +187,36 @@ const getFormErrorMessage = (name) => {
                     <div className="flex">
                         <p className="col-4 font-semibold m-0 p-1">Tax Amount($):</p>
                         <p className="col-8 m-0 p-1">
-                            <InputText id="taxAmount" placeholder="Enter Tax Amount" value={formik.values.taxAmount} onChange={formik.handleChange} className="h-3rem w-7 border-round-xs" />
+                            <InputText id="taxAmount" placeholder="Enter Tax Amount" value={formik.values.taxAmount} onChange={formik.handleChange} onBlur={formik.handleBlur}   className={classNames({ "p-invalid": isFormFieldValid("taxAmount") }, "h-3rem w-21rem border-round-xs mr-3")} />
+                            {getFormErrorMessage("taxAmount")}
                         </p>
                     </div>
                     <div className="flex">
                         <p className="col-4 font-semibold m-0 p-1">Tax Breakup($):</p>
                         <p className="col-8 m-0 p-1">
-                            <InputText id="taxBreakup" placeholder="Enter Tax Breakup" value={formik.values.taxBreakup} onChange={formik.handleChange} className="h-3rem w-7 border-round-xs" />
+                            <InputText id="taxBreakup" placeholder="Enter Tax Breakup" value={formik.values.taxBreakup} onChange={formik.handleChange} onBlur={formik.handleBlur} className={classNames({ "p-invalid": isFormFieldValid("taxBreakup") }, "h-3rem w-21rem border-round-xs mr-3")} />
+                            {getFormErrorMessage("taxBreakup")}
                         </p>
                     </div>
                     <div className="flex">
                         <p className="col-4 font-semibold m-0 p-1">Invoice Amount($):</p>
                         <p className="col-8 m-0 p-1">
-                            <InputText id="invoiceAmount" placeholder="Enter Invoice Amount" value={formik.values.invoiceAmount} onChange={formik.handleChange} className="h-3rem w-7 border-round-xs" />
+                            <InputText id="invoiceAmount" placeholder="Enter Invoice Amount" value={formik.values.invoiceAmount} onChange={formik.handleChange} onBlur={formik.handleBlur} className={classNames({ "p-invalid": isFormFieldValid("invoiceAmount") }, "h-3rem w-21rem border-round-xs mr-3")} />
+                            {getFormErrorMessage("invoiceAmount")}
                         </p>
                     </div>
                 </div>
-            </Dialog>
+                <div className="flex justify-content-between m-3" >
+                <Button label="Close" onClick={() => setAdHocInvoiceModal(false)} />
+                <Button label="Submit" type="submit" /> 
+
+               
+                </div>
+           
+           
         </div>
+        </form>
+        </>
     );
 };
 
--- src/app/features/screens/billing_and_invoices/pages/InvoicePage.js
+++ src/app/features/screens/billing_and_invoices/pages/InvoicePage.js
@@ -1,52 +1,28 @@
 import React, { useEffect, useState } from "react";
-import PlanInfo from "../components/PrepaidPlanInfo";
 import InvoiceTable from "../components/InvoiceTable";
-/*
-import InvoiceTypes from "../components/InvoiceTypes"; 
-import DetailedTransactionModal from "../components/modals/DetailedTransactionModal";
-import NsfModal from "../components/modals/NsfModal";
-import AdHocModal from "../components/modals/AdHocModal";
-import MismatchInvoiceModal from "../components/modals/MismatchInvoiceModal";
-import EbillModal from "../components/modals/EbillModal"; 
-import MismatchBillModal from "../components/modals/MismatchBillModal";
-import AdjustBalanceModal from "../components/modals/AdjustBalanceModal";
-import PayInvoiceModal from "../components/modals/PayInvoiceModal";
-import DiscountCreditModal from "../components/modals/DiscountCreditModal";
-import AdjustWalletModal from "../components/modals/AdjustWalletModal";
-import AddWalletModal from "../components/modals/AddWalletModal";
-import PaymentModal from "../components/modals/PaymentModal";
-import PaymentDetailModal from "../components/modals/PaymentDetailModal"; 
 
-import PrepaidEditabaleInvoices from "../components/PrePaidEditableInvoices";*/
+import AdHocModal from "../components/modals/AdHocModal";
 import BillingNavbar from "../../customer_profile/modals/BillingNavbar";
 import Axios from "axios";
 import { Card } from "primereact/card";
-import { Await, useLocation } from 'react-router-dom';
-import PrepaidEditabaleInvoices from "../components/PrePaidEditableInvoices";
+import {  useLocation } from 'react-router-dom';
+
 import ChangeCustomerStatus from "../../customer_profile/change_customer_status/change_customer_status";
 import { Dialog } from "primereact/dialog";
+
+import { Button } from "primereact/button";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const InvoicePage = () => {
-    //const [detailedTransactionModal, setDetailedTransactionModal] = useState(false);
-    // const [nsfModal, setNsfModal] = useState(false);  
+   
     const [accountType, setAccountType] = useState("")
     const [invoices, setInvoices] = useState([])
     const [changeCustomerStatusDialog, setChangeCustomerStatus] = useState(false);
-    // const [adHocInvoiceModal, setAdHocInvoiceModal] = useState(false);
-    // const [misMatchInvoiceModal, setMisMatchInvoiceModal] = useState(false);
-    //const [ebillModal, setEbillModal] = useState(false);
-    //const [misMatchBillModal, setMisMatchBillModal] = useState(false);
-    // const [adjustBalanceModal, setAdjustBalanceModal] = useState(false);
-    // const [payInvoiceModal, setPayInvoiceModal] = useState(false);
-    //  const [discountCreditModal, setDiscountCreditModal] = useState(false);
-    // const [adjustWalletModal, setAdjustWalletModal] = useState(false); 
-    /*setDetailedTransactionModal={setDetailedTransactionModal} */
-    // const [addWalletModal, setAddWalletModal] = useState(false);
-    // const [paymentModal, setPaymentModal] = useState(false); 
+     const [adHocInvoiceModal, setAdHocInvoiceModal] = useState(false);
+    
     const [currentPlan, setCurrentPlan] = useState([])
-    //const [paymentDetailModal, setPaymentDetailModal] = useState(false);
+   
     const [userDetails, setUserDetails] = useState()
     const [cpData, setCpData] = useState([]);
 
@@ -88,11 +64,23 @@ const InvoicePage = () => {
     }, [selectedId])
 
     return (
+        <>
+
+       
         <Card>
             <BillingNavbar setChangeCustomerStatus={setChangeCustomerStatus} changeCustomerStatusDialog={changeCustomerStatusDialog} />
             <Dialog draggable={false} visible={changeCustomerStatusDialog} header={`Change Customer Status`} style={{ width: "70vw" }} onHide={() => setChangeCustomerStatus((prev) => !prev)}>
                 <ChangeCustomerStatus cpData={cpData} setChangeCustomerStatus={setChangeCustomerStatus} />
             </Dialog>
+            <Dialog draggable={false} visible={adHocInvoiceModal} header={`Add Adhoc Invoice`} style={{ width: "50vw" }} onHide={() => setAdHocInvoiceModal((prev) => !prev)}>
+            <AdHocModal cpData={cpData} adHocInvoiceModal={adHocInvoiceModal} setAdHocInvoiceModal={setAdHocInvoiceModal} />
+            </Dialog>
+           
+               
+           
+            <div className="mt-5 mb-5 ml-5">  
+<Button label="AD HOC Invoice" text raised onClick={()=>{setAdHocInvoiceModal(true)}}/>
+        </div>
             <div className=" p-0 m-3 card">
                 {/* <PlanInfo  /> */}
                 <div className="mx-4">
@@ -106,60 +94,13 @@ const InvoicePage = () => {
                 <InvoiceTable onAPISuccess={handleAPISuccess} userDetails={userDetails} className="mb-3" invoiceData={invoices} />
 
             </div>
-            {/*
+          
     
 
-            <div className=" p-0 m-3 card">
-               
-                <div className=" border-round p-3  mx-4 mt-0">
-                    <p className="font-bold text-xl ">Invoices</p>
-                </div>
-                
-                <InvoiceTypes    
-                accountType={accountType} 
-                setNsfModal={setNsfModal}
-                setAdHocInvoiceModal={setAdHocInvoiceModal}
-                setMisMatchInvoiceModal={setMisMatchInvoiceModal}
-                setEbillModal={setEbillModal}
-                setMisMatchBillModal={setMisMatchBillModal}
-                setAdjustBalanceModal={setAdjustBalanceModal}
-                setPayInvoiceModal={setPayInvoiceModal}
-                setDiscountCreditModal={setDiscountCreditModal}
-                setAdjustWalletModal={setAdjustWalletModal}
-                setAddWalletModal={setAddWalletModal}
-            />  
-                <Prepaid setPaymentModal={setPaymentModal} />
-                <PrepaidEditabaleInvoices  setPaymentModal={setPaymentModal} />
-               <div>
-                    <DetailedTransactionModal detailedTransactionModal={detailedTransactionModal} setDetailedTransactionModal={setDetailedTransactionModal} />
-                    <NsfModal nsfModal={nsfModal} setNsfModal={setNsfModal} />
-                    <AdHocModal adHocInvoiceModal={adHocInvoiceModal} setAdHocInvoiceModal={setAdHocInvoiceModal} />
-                    <MismatchInvoiceModal misMatchInvoiceModal={misMatchInvoiceModal} setMisMatchInvoiceModal={setMisMatchInvoiceModal} />
-                    <EbillModal ebillModal={ebillModal} setEbillModal={setEbillModal} />
-                    <MismatchBillModal misMatchBillModal={misMatchBillModal} setMisMatchBillModal={setMisMatchBillModal} />
-                    <AdjustBalanceModal adjustBalanceModal={adjustBalanceModal} setAdjustBalanceModal={setAdjustBalanceModal} />
-                    <AdjustWalletModal adjustWalletModal={adjustWalletModal} setAdjustWalletModal={setAdjustWalletModal} />
-                    <PayInvoiceModal payInvoiceModal={payInvoiceModal} setPayInvoiceModal={setPayInvoiceModal} />
-                    <DiscountCreditModal discountCreditModal={discountCreditModal} setDiscountCreditModal={setDiscountCreditModal} />
-                    <AddWalletModal addWalletModal={addWalletModal} setAddWalletModal={setAddWalletModal} />
-                    <PaymentModal paymentModal={paymentModal} setPaymentModal={setPaymentModal} setPaymentDetailModal={setPaymentDetailModal} />
-                    <PaymentDetailModal paymentDetailModal={paymentDetailModal} setPaymentDetailModal={setPaymentDetailModal} />
-                </div> 
-                <div className="mx-4">
-                    <p className="m-0 text-xs font-bold" style={{ color: "red" }}>
-                        •Row in red color are unpaid invoices
-                    </p>
-                    <p className="text-xs font-bold" style={{ color: "blue" }}>
-                        •Row in blue color are paid invoices
-                    </p>
-                </div>
-                <br />
-                <InvoiceTable userDetails={userDetails} className="mb-3" invoiceData={invoices} setDetailedTransactionModal={setDetailedTransactionModal} />
-                
-            </div>   
-    */}
+           
 
         </Card>
+        </>
     );
 };
 
