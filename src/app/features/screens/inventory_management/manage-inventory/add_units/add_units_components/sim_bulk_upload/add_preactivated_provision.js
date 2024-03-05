28146b57f635ecaad32da4b1d2ba5f1fd9ece18d
Zaka Ullah <mrzakaullah01@gmail.com>
2024-03-05 16:19:18 +0500

    SIM Inventory Sample File added

--- src/app/features/screens/inventory_management/manage-inventory/add_units/add_units_components/sim_bulk_upload/add_preactivated_provision.js
+++ src/app/features/screens/inventory_management/manage-inventory/add_units/add_units_components/sim_bulk_upload/add_preactivated_provision.js
@@ -300,7 +300,7 @@ export default function SIMBulkUploadAddPreActivatedProvision({ permissions,unit
                                 &nbsp; (Sample file)
                             </p>
                         ) : (
-                            <a download={true} href="/images/inventory Sample File.xlsx" className="font-bold">
+                            <a download={true} href="/images/Sample File(Sim Bulk Upload).xlsx" className="font-bold">
                                 {" "}
                                 &nbsp; (Sample file)
                             </a>
